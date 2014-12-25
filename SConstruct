5bfc5e8858c751761d9d8f1ed695a4c415a2a8d0
Pieter Deelen <pieter.deelen@gmail.com>
2014-12-25 11:04:19 +0100

    Add build for tests.

--- SConstruct
+++ SConstruct
@@ -8,7 +8,12 @@ Alias('release', 'build/release')
 Alias('all', ['build/debug', 'build/release'])
 
 def build(env, buildDir):
-    env.VariantDir(buildDir, 'src', duplicate=1	)
+    env.VariantDir(buildDir, 'src', duplicate=1)
+    Export('env')
+    SConscript(os.path.join(buildDir, 'SConscript'))
+
+def build_test(env, buildDir):
+    env.VariantDir(buildDir, 'test', duplicate=1)
     Export('env')
     SConscript(os.path.join(buildDir, 'SConscript'))
 
@@ -17,6 +22,11 @@ debug = Environment(CPPPATH=['#/src'])
 debug.Append(CCFLAGS = '-std=c++14 -O0 -g -Wall')
 debug.Append(LINKFLAGS = '-g')
 build(debug, 'build/debug')
+
+test = Environment(CPPPATH=['#/src', '#/test', '#/external'])
+test.Append(CCFLAGS = '-std=c++14 -O0 -g -Wall')
+test.Append(LINKFLAGS = '-g')
+build_test(test, 'build/test')
 
 # Release build.
 release = Environment(CPPPATH=['#/src'])
---
+++ test/vector/Vector_test.cpp
@@ -0,0 +1,55 @@
+#include "vector/Vector.h"
+#include <gtest/gtest.h>
+
+class Vector_test : public testing::Test {
+public:
+	Vector3d v;
+
+	virtual void SetUp() {
+		v = Vector3d(1, 2, 3);
+	}
+
+};
+
+TEST_F(Vector_test, testAccessors)
+{
+	ASSERT_EQ(1, v[0]);
+	ASSERT_EQ(2, v[1]);
+	ASSERT_EQ(3, v[2]);
+}
+
+TEST_F(Vector_test, testAssignment)
+{
+	Vector3d w = v;
+
+	ASSERT_EQ(1, w[0]);
+	ASSERT_EQ(2, w[1]);
+	ASSERT_EQ(3, w[2]);
+}
+
+TEST_F(Vector_test, testEquals)
+{
+	Vector3d v2(1, 2, 3);
+	Vector3d w(3, 2, 1);
+
+	ASSERT_EQ(v, v2);
+	ASSERT_EQ(v, v2);
+	ASSERT_EQ(v2, v);
+	ASSERT_NE(v, w);
+	ASSERT_NE(w, v);
+}
+
+TEST_F(Vector_test, testPlus)
+{
+	Vector3d w(4, 5, 6);
+	Vector3d u = v + w;
+	ASSERT_EQ(5, u[0]);
+	ASSERT_EQ(7, u[1]);
+	ASSERT_EQ(9, u[2]);
+}
+
+TEST_F(Vector_test, testDot)
+{
+	Vector3d w(4, 5, 6);
+	ASSERT_EQ(32.0, dot(v, w));
+}
